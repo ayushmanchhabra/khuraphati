0fef194afe245332bb819acad5c3d5fd4da417f2
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-03-17 22:24:03 +0530

    Extract apk(s) from device using adb

---
+++ README.md
@@ -0,0 +1,27 @@
+# Khuraphati
+
+A collection of miscellaneous scripts.
+
+## Getting started
+
+1. Clone this repo `git clone https://github.com/ayushmanchhabra/khuraphati.git`
+1. Jump into repo `cd ./khuraphati`
+1. Make script executable `chmod +x ./aos.sh`
+1. Follow below usage instructions
+
+## aos.sh
+
+```sh
+└─$ ./aos.sh -h
+aos.sh <command>
+
+Usage:
+
+./aos.sh extract com.package.name /path/to/outDir Extract APK(s) from Android device
+```
+
+## License
+
+MIT.
+
+> Legal disclaimer: Usage of khuraphati scripts on hosts without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program.
---
+++ aos.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ "$1" == '-h' || "$1" == '--help' ]]; then
+    echo -e "aos.sh <command>\n"
+    echo -e "Usage:\n"
+    echo "./aos.sh extract com.package.name /path/to/outDir Extract APK(s) from Android device using Android Debug Bridge (adb)"
+    exit 0
+elif [[ "$1" == "-e" || "$1" == "--extract" ]]; then
+    extract_apk "$2" "$3"
+else
+    echo "Unknown command. Please consult the -h or --help command for guidance."
+fi
+
+# Extract APKs from Android device using Android Debug Bridge (adb).
+function extract_apk () {
+    # Package name of target APK(s)
+    PKG_NAME=$1
+    # Directory in which to extract target APK(s)
+    OUT_DIR=$2
+    # Store output of adb command in a variable
+    PKG_PATH=$( adb shell pm path "$PKG_NAME" )
+    # Sort the output by \n and store in array
+    mapfile -t PKG_PATHS <<< "$PKG_PATH"
+    # Extract APK(s) into OUT_DIR directory.
+    for PATH in "${PKG_PATHS[@]}"; do
+	    adb pull "$PATH#package: $OUT_DIR"
+    done
+}

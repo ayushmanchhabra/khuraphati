917c71b613b426eb97ad85193f89bcc500b3f0c6
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-03-18 11:41:18 +0530

    Input adb executable file path

--- aos.sh
+++ aos.sh
@@ -1,29 +1,31 @@
 #!/bin/bash
 
-if [[ "$1" == 'h' || "$1" == 'help' ]]; then
-    echo -e "aos.sh <command>\n"
-    echo -e "Usage:\n"
-    echo "./aos.sh (h)elp ..Get information on how to use this script"
-    echo "./aos.sh (e)xtract com.package.name /path/to/outDir ..Extract APK(s) from Android device using Android Debug Bridge (adb)"
-    exit 0
-elif [[ "$1" == "e" || "$1" == "extract" ]]; then
-    extract_apk "$2" "$3"
-else
-    echo "Unknown command. Please consult the -h or --help command for guidance."
-fi
-
 # Extract APKs from Android device using Android Debug Bridge (adb).
 function extract_apk () {
     # Package name of target APK(s)
     PKG_NAME=$1
     # Directory in which to extract target APK(s)
     OUT_DIR=$2
+    # File path to Android Debug Bridge (adb) binary
+    ADB_EXEC=$3
     # Store output of adb command in a variable
-    PKG_PATH=$( adb shell pm path "$PKG_NAME" )
+    PKG_PATH=$( "$ADB_EXEC" shell pm path "$PKG_NAME" | sed 's/package://g' | tr -d '\r' )
     # Sort the output by \n and store in array
     mapfile -t PKG_PATHS <<< "$PKG_PATH"
     # Extract APK(s) into OUT_DIR directory.
     for PATH in "${PKG_PATHS[@]}"; do
-	    adb pull "$PATH#package: $OUT_DIR"
+	    "$ADB_EXEC" pull "$PATH" "$OUT_DIR"
     done
 }
+
+if [[ "$1" == 'h' || "$1" == 'help' ]]; then
+    echo -e "aos.sh <command>\n"
+    echo -e "Usage:\n"
+    echo "./aos.sh (h)elp ..Get information on how to use this script"
+    echo "./aos.sh (e)xtract com.package.name /path/to/outDir /path/to/adb/exe ..Extract APK(s) from Android device using Android Debug Bridge (adb)"
+    exit 0
+elif [[ "$1" == "e" || "$1" == "extract" ]]; then
+    extract_apk "$2" "$3" "$4"
+else
+    echo "Unknown command. Please consult the h or help command for guidance."
+fi
